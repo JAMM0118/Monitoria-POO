546b16774f0eea8a5b9abe54296231a9ba459a2e
JAMM0118 <jhon.alejandro.correounivalle.edu.co>
2023-09-06 18:19:59 -0500

    First commit

---
+++ Bienvenidad a C++/helloWorld.cpp
@@ -0,0 +1,12 @@
+// link del video para tener c++ en visual studio code
+//https://www.youtube.com/watch?v=v3ENcQpoA5A&t=6s
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"Hello World"<<endl;
+    
+    return 0;
+}
