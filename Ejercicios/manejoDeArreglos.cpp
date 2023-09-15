04d0a058f652e296efaf93d7d4457f188dc7605c
JAMM0118 <jhon.alejandro.correounivalle.edu.co>
2023-09-14 21:39:50 -0500

    Nuevos ejercicios

---
+++ Ejercicios/manejoDeArreglos.cpp
@@ -0,0 +1,11 @@
+/*En un arreglo unidimensional se almacenan las calificaciones de N alumnos. 
+Construya un algoritmo que permita calcular e imprimir lo siguiente:
+
+a. El promedio general del grupo
+
+b. Número de alumnos aprobados y número de alumnos reprobados 
+(Si el alumno sacó una calificación menor a 3 se le considera reprobado).
+
+c. Porcentaje de alumnos aprobados y reprobados
+
+d. Número de alumnos cuya calificación fue mayor a 4.5*/
---
+++ Ejercicios/manejo_de_arreglo_con_funciones.cpp
@@ -0,0 +1,9 @@
+/*En un arreglo unidimensional se ha almacenado el número total de toneladas de cereales cosechadas
+ durante cada mes del año anterior. Construya un algoritmo que proporcione la siguiente información:
+
+a. El promedio anual de toneladas cosechadas
+b. ¿Cuántos meses tuvieron una cosecha superior al promedio anual?
+c. ¿Cuántos meses tuvieron una cosecha inferior al promedio anual?
+d. ¿Cuál fue el mes en el que se produjeron mayor número de toneladas?
+
+*/
